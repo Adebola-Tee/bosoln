17dd7c0c027f351a0b09ef98b80cad2177b53b94
Adebola-Tee <taiwooloyede49@gmail.com>
2023-05-24 20:28:58 +0100

    modified onFocus and onBlur on the input fields on the 3 files

--- signin.js
+++ signin.js
@@ -1,3 +1,15 @@
+function changeBorderColor() {
+   document.getElementById("name-text").style.borderColor = "purple";
+ }
+ function toDefaultColor() {
+   document.getElementById("name-text").style.borderColor = "";
+ }
+ function changeBorderColor2() {
+   document.getElementById("signin-password").style.borderColor = "purple";
+ }
+ function toDefaultColor2() {
+   document.getElementById("signin-password").style.borderColor = "";
+ }
 let passwordToggle = document.getElementById("password-toggle");
 let passwordField = document.getElementById("signin-password");
 function togglePasswordVisibility(){
